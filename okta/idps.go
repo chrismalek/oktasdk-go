c90c332883b037bc4b8db7a15c7a16e20ea1fd95
Purrbiscuit <spurr@articulate.com>
2018-08-13 11:34:39 -0400

    finish IdentityProvider struct in idps.go

--- okta/idps.go
+++ okta/idps.go
@@ -36,4 +36,48 @@ type IdentityProvider struct {
 			} `json:"client"`
 		} `json:"credentials"`
 	} `json:"protocol"`
+	Policy struct {
+		Provisioning struct {
+			Action string `json:"action"`
+			ProfileMaster bool `json:"profileMaster"`
+			Groups struct {
+				Action string `json:"action"`
+			} `json:"groups"`
+			Conditions struct {
+				Deprovisioned struct {
+					Action string `json:"action"`
+				} `json:"deprovisioned"`
+				Suspended struct {
+					Action string  `json:"action"`
+				} `json:"suspended"`
+			} `json:"conditions"`
+		} `json:"provisioning"`
+		AccountLink struct {
+			filter string  `json:"filter"`
+			action string  `json:"action"`
+		} `json:"accountLink"`
+		Subject struct {
+			UserNameTemplate struct {
+					Template "string"  `json:"template"`
+				} `json:"userNameTemplate"`
+			Filter string  `json:"filter"`
+			MatchType string  `json:"matchType"`
+		} `json:"subject"`
+		MaxClockSkew int  `json:"maxClockSkew"`
+	} `json:"policy"`
+	Links struct {
+		Authorize struct {
+			Href string  `json:"href"`
+			Templated bool  `json:"templated"`
+			Hints struct {
+				Allow []interface{} `json:"allow"`
+			} `json:"hints"`
+		} `json:"authorize"`
+		ClientRedirectUri struct {
+			Href string `json:"href"`
+			Hints struct {
+				Allow []interface{} `json:"allow"`
+			} `json:"hints"`
+		} `json:"clientRedirectUri"`
+	} `json:"_links"`
 }
