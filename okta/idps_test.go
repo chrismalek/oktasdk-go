decb973223a5857a6e526e4b1ed7d3946939477d
Purrbiscuit <spurr@articulate.com>
2018-08-13 14:18:02 -0400

    add setupTestIdentityProvider function to idps_test.go

--- okta/idps_test.go
+++ okta/idps_test.go
@@ -1,1 +1,27 @@
 package okta
+
+import (
+	"testing"
+	"time"
+)
+
+var testIdentityProvider *IdentityProvider
+
+func setupTestIdentityProvider() {
+	hmm, _ := time.Parse("2006-01-02T15:04:05.000Z", "2018-02-16T19:59:05.000Z")
+
+	testIdentityProvider = &IdentityProvider{
+		ID:          "0oa62bfdiumsUndnZ0h7",
+		Type:        "GOOGLE",
+		Status:      "ACTIVE",
+		Name:        "Google",
+		Created:     hmm,
+		LastUpdated: hmm,
+	}
+}
+
+func TestIdentityProvider(t *testing.T) {
+	setup()
+	defer teardown()
+	setupTestIdentityProvider()
+}
